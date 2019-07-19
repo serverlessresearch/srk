3e8e0758e10f67fb18f48ccde7b034f87418700b
Johann Schleier-Smith <jssmith@gmail.com>
2019-07-19 11:49:35 -0700

    simple plotting of metric aggregates

--- tools/plot.py
+++ tools/plot.py
@@ -4,23 +4,30 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot Serverless Data.')
     parser.add_argument('--experiment-id', required=True, help='which experiment to print')
     parser.add_argument('--log', default='log.txt', help='log file to plot')
+    parser.add_argument('--metric-value', default='concurrency', help='what metric to plot')
     args = parser.parse_args()
 
     experiment_id = args.experiment_id
 
     run_intervals = []
+    reports = {}
     with open(args.log) as f:
         for line in f.readlines():
             event = json.loads(line)
             if "uuid" in event and event["uuid"].startswith(experiment_id):
                 if event["action"] == "end":
-                    run_intervals.append((event["begin_time"], event["end_time"]))
-    min_start = min([x[0] for x in run_intervals])
-    max_end = max([x[1] for x in run_intervals])
+                    r = (event["uuid"], event["begin_time"], event["end_time"], {})
+                    run_intervals.append(r)
+                elif event["action"] == "report":
+                    reports[event["uuid"]] = event["data"]
+
+    min_start = min([x[1] for x in run_intervals])
+    max_end = max([x[2] for x in run_intervals])
 
     experiment_len = max_end - min_start
 
@@ -28,22 +35,38 @@ if __name__ == "__main__":
 
     plot_n = int(experiment_len / plot_interval) + 1
 
-    concurrency = [0] * plot_n
-    for (start, end) in run_intervals:
+    def update_concurrency(vec, index, uuid, dt):
+        vec[index] += 1
+
+    def update_metric_sum(vec, index, uuid, dt):
+        if uuid in reports:
+            data = reports[uuid]
+            if args.metric_value in data:
+                vec[index] += data[args.metric_value] / dt
+            else:
+                print("missing key %s in report for uuid %s" % (args.metric_value, ))
+        else:
+            print("missing report for uuid %s" % uuid)
+
+    if args.metric_value == 'concurrency':
+        update_fn = update_concurrency
+        metric_label = 'concurrency'
+    else:
+        update_fn = update_metric_sum
+        metric_label = "%s (1/s)" % args.metric_value
+
+
+    v = [0] * plot_n
+    for (uuid, start, end, data) in run_intervals:
         start_int = int((start - min_start) / plot_interval)
         end_int = int((end - min_start) / plot_interval)
         for i in range(start_int, end_int):
-            concurrency[i] += 1
+            update_fn(v, i, uuid, end - start)
     t = np.arange(0, experiment_len, plot_interval)
-    # print(min_start, max_end, experiment_len, plot_n)
-    # print(len(concurrency))
-    # print(len(t))
+
     fig, ax = plt.subplots()
-    ax.plot(t, concurrency)
-    ax.set(xlabel='time (s)', ylabel='concurrency')
+    ax.plot(t, v)
+    ax.set(xlabel='time (s)', ylabel=metric_label)
     ax.grid()
-    fig.savefig('%s-concurrency.pdf' % experiment_id)
+    fig.savefig('%s-%s.pdf' % (experiment_id, args.metric_value))
     # plt.show()
-
-
-
