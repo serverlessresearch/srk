3ad688243ec5b22b8895f53cdf31491d407692ec
Nathan Pemberton <nathanp@berkeley.edu>
2019-09-17 14:04:28 -0700

    Update comments in example hello world program to indicate our intention to fix the incompatibility between OL and AWS.

--- examples/echo/f.py
+++ examples/echo/f.py
@@ -1,3 +1,5 @@
+# This is a compatiblity shim for Open Lambda. Eventually, we will likely make
+# OpenLambda compatible with AWS Lambda and remove the need for this
 import echo
 def f(event):
     return echo.echo(event)
