211e17f12e2ed9269dffd7688b9bd1639d563899
Libo Chen <clb729@gmail.com>
2019-10-14 02:47:48 +0000

    update objstore protobuf file; add objstore.cpp

--- scripts/pb_compile.sh
+++ scripts/pb_compile.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# golang
 protoc -I pkg/objstore pkg/objstore/objstore.proto --go_out=plugins=grpc:pkg/objstore
+# c++
+protoc -I pkg/objstore --cpp_out=pkg/objstore/ pkg/objstore/objstore.proto
+protoc -I pkg/objstore --grpc_out=pkg/objstore/ --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` pkg/objstore/objstore.proto
