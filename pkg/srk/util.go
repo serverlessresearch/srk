f70c2347862b057665f20621b2022a0202913809
Nathan Pemberton <nathanp@berkeley.edu>
2020-02-06 14:45:58 -0500

    Switch to native CopyFile() routine in openlambda. This introduces the pkg/srk/util.go file that adds common helper functions to package srk.

---
+++ pkg/srk/util.go
@@ -0,0 +1,37 @@
+package srk
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func CopyFile(src, dst string) error {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file", src)
+	}
+
+	from, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer from.Close()
+
+	to, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE, sourceFileStat.Mode().Perm())
+	if err != nil {
+		return err
+	}
+	defer to.Close()
+
+	_, err = io.Copy(to, from)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
